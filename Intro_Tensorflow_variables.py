bea89c18d9152f05d272430b66e0f69da2261ba6
Shuya Fuchigami <>
2020-05-14 16:15:42 +0200

    some fix

--- Intro_Tensorflow_variables.py
+++ Intro_Tensorflow_variables.py
@@ -1,26 +1,28 @@
 import tensorflow as tf
 
 
-def forward_pass(w,x):
-    return tf.mutmul(w,x)
+def forward_pass(w, x):
+    return tf.matmul(w, x)
 
 
 def train_loop(x, niter=5):
-    with tf.variable_scope("model", reuse=tf.AUTO_REUSE):
-        w = tf.get_variable("weights",
-                            shape=(1, 2),
-                            initializer=tf.truncated_normal_initializer(),
-                            trainable=True)
+    with tf.compat.v1.variable_scope("model", reuse=tf.compat.v1.AUTO_REUSE):
+        w = tf.compat.v1.get_variable("weights",
+                                      shape=(1, 2),
+                                      initializer=tf.compat.v1.truncated_normal_initializer(),
+                                      trainable=True)
     preds = []
-    for k in xrange(niter):
+    for k in range(niter):
         preds.append(forward_pass(w, x))
         w = w + 0.1
     return preds
 
 
 with tf.compat.v1.Session() as sess:
-    predictions = train_loop(tf.constant[[3.2, 5.1, 7.2], [4.3, 6.2, 8.3]])
+    predictions = train_loop(tf.constant([[3.2, 5.1, 7.2],
+                                          [4.3, 6.2, 8.3]]))
     tf.compat.v1.global_variables_initializer().run()
-    for i in xrange(len(predictions)):
-        print("{}:{}".format(i, predictions[i].eval()))
 
+    # predictions = [1, 2, 3]
+    for i in range(len(predictions)):
+        print("{}:{}".format(i, predictions[i].eval()))
